b91d7a82f304bb134042c1b49e21bef58e2c4338
salexpro <dev@salex.pro>
2017-09-12 01:30:56 +0300

    dropdowns

---
+++ source/js/app.js
@@ -0,0 +1,10 @@
+/* 
+@codekit-prepend '../../bower_components/jquery/dist/jquery.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.core.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.keyboard.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.triggers.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.dropdown.min.js';
+*/
+
+$(document).foundation();
