d44d277a679ff88e9f38dc323f657c7cc72394f6
salexpro <dev@salex.pro>
2017-09-12 06:56:34 +0300

    popups

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,13 @@
 /* 
 @codekit-prepend '../../bower_components/jquery/dist/jquery.min.js';
 @codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.core.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.min.js';
 @codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.box.min.js';
 @codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.keyboard.min.js';
 @codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.triggers.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.util.motion.min.js';
 @codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.dropdown.min.js';
+@codekit-prepend '../../bower_components/foundation-sites/dist/js/plugins/foundation.reveal.min.js';
 */
 
 $(document).foundation();
