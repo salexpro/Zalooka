4e08eab017c4e520080c2081136e8a218442550f
Alexander <dev@salex.pro>
2018-10-17 19:01:19 +0300

    profile/reservations_add page

--- src/js/components/fastnav.js
+++ src/js/components/fastnav.js
@@ -11,4 +11,4 @@ const pageWidget = pages => {
     });
 }
 
-pageWidget(['404', 'index', 'articles/index', 'articles/article', 'cities', 'city', 'contacts', 'country', 'faq', 'pricelist', 'profile/reservations', 'profile/reservations_empty', 'reviews/index', 'reviews/countries', 'reviews/country', 'reviews/city', 'shopping', 'spa', 'taxi', 'tours', 'tour', 'transfer', 'visa']);
+pageWidget(['404', 'index', 'articles/index', 'articles/article', 'cities', 'city', 'contacts', 'country', 'faq', 'pricelist', 'profile/reservations', 'profile/reservations_empty', 'profile/reservations_add', 'reviews/index', 'reviews/countries', 'reviews/country', 'reviews/city', 'shopping', 'spa', 'taxi', 'tours', 'tour', 'transfer', 'visa']);
